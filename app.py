052cc131560167a6ac33aa5d59aeb055215efc1f
Invisoguard <zubin@pikateck.com>
2025-04-14 14:33:37 +0530

    backend/app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__, static_folder='../', template_folder='../')
+app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
-
-@app.route('/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('../', filename)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
