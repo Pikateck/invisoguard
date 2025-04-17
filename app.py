dc0ef8285f0c1879a894b0ddb364da6ae5157af1
Invisoguard <zubin@pikateck.com>
2025-04-17 14:07:03 +0530

    InvisoGuard_Full_Deployment_Fixed

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    user = User(id="1")
+    login_user(user)
+    return redirect(url_for('dashboard'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+if __name__ == '__main__':
+    app.run()
