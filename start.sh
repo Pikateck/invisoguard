fb9262b70208061e470370b1e2e89c530a0e9c49
Invisoguard <zubin@pikateck.com>
2025-04-21 12:38:50 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install https://github.com/RPi-Distro/python-dlib/releases/download/v19.24.0/dlib-19.24.0-cp311-cp311-manylinux_2_17_x86_64.whl
-pip install face_recognition opencv-python reportlab
-pip install -r requirements.txt
-python app.py
